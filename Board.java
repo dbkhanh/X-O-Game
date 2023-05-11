9bc7140ecae25afcbf2aebddb898e319bcf895ff
dbkhanh <db2.khanh@gmail.com>
2023-05-11 14:42:16 -0400

    Second commit

--- Board.java
+++ Board.java
@@ -1,13 +1,55 @@
 package Game;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class Board extends JPanel {
+    private static final int N = 3;
+    private static final int M = 3;
+    private Image imgX;
+    private Image imgO;
+
+    public Board(){
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+
+                int x = e.getX();
+                int y = e.getY();
+            }
+        });
+
+        try {
+            imgX = ImageIO.read(getClass().getResource("x.png"));
+            imgO = ImageIO.read(getClass().getResource("o.png"));
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
     public void paint(Graphics g){
         int w = getWidth()/3;
         int h = getHeight()/3;
         Graphics2D graphics2D = (Graphics2D) g;
         graphics2D.setColor(Color.BLUE);
-        graphics2D.fillRect(0,0, w, h);
+
+        int k = 0;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                int x = i * w;
+                int y = j * h;
+
+                Color color = k%2 == 0? Color.BLUE: Color.RED;
+                graphics2D.setColor(color);
+                graphics2D.fillRect(x,y,w,h);
+
+                Image img = k%2 == 0? imgX: imgO;
+                graphics2D.drawImage(img,x,y,w-1,h-1, this);
+                k++;
+            }
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -6,19 +6,34 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args){
-        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        JPanel jPanel = new JPanel();
+        BoxLayout boxLayout = new BoxLayout(jPanel, BoxLayout.Y_AXIS);
+        jPanel.setLayout(boxLayout);
 
         Board board = new Board();
+        board.setPreferredSize(new Dimension(300, 300));
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setPreferredSize(new Dimension(300, 50));
+        bottomPanel.setBackground(Color.YELLOW);
+
+        jPanel.add(board);
+        jPanel.add(bottomPanel);
+        //Box layout, Flow layout, Border layout
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+
+
         JFrame jFrame = new JFrame("Tic-Tac-Toe");
-        jFrame.setSize(600, 600);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setResizable(false);
-        jFrame.add(board);
+        jFrame.setResizable(true);
+        jFrame.add(jPanel);
 
         int x = (int) (dimension.getWidth()/2 - jFrame.getWidth()/2);
         int y = (int) (dimension.getHeight()/2 - jFrame.getHeight()/2);
 
         jFrame.setLocation(x, y);
+
+        jFrame.pack();
         jFrame.setVisible(true);
     }
 }
